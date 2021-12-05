6d1f6553ee356419ea0152356680332a4e4e8bb2
Ldelillo <95347934+Ldelillo@users.noreply.github.com>
2021-12-05 16:23:07 +0100

    Add files via upload

---
+++ Code Advent 1b.py
@@ -0,0 +1,11 @@
+
+with open('./input1', 'r') as numsInput:
+    list = (numsInput.read()).split("\n")
+    res = 0
+    sum = int(list[0]) + int(list[1]) + int(list[2])
+    for x in range(3, len(list)):
+        newSum = int(list[x]) + int(list[x-1]) + int(list[x-2])
+        if newSum > sum:
+            res += 1
+        sum = newSum
+print(res)
